510069747db2b8560b6b1ab47d9b15d6066d8bca
kacperkaszak <kkaszak1@wp.pl>
2014-04-12 13:57:15 +0200

    hello

--- oddzial.h
+++ oddzial.h
@@ -5,34 +5,34 @@
 class oddzial
 {
 	public:
-/*	dodajUcznia - dodaje ucznia do oddzialu jesli jest miejsce, 
+/*	dodajUcznia - dodaje ucznia do oddzialu jesli jest miejsce,
 	Arg.we:
 	uczen kandydat - obiekt klasy uczen; pobiera od ucznia dane o jezyku, punktach i preferencjach
 	Zwracane:
-	true - udalo sie przypisac ucznia do klasy, 
+	true - udalo sie przypisac ucznia do klasy,
 	false - nie udalo sie
-*/	
+*/
 	bool dodajUcznia (Uczen kandydat);
-	
-	
-/*	wypiszListeUczniow - eksportuje liste uczniow do pliku zewnetrznego, 
+
+
+/*	wypiszListeUczniow - eksportuje liste uczniow do pliku zewnetrznego,
 	Arg.we:
 	oddzial_csv - nazwa pliku, w ktorym zostana zapisani uczniowie z danej klasy
 	Zwracane:
-	true - udalo sie wyeksportowac dane do pliku zewnetrzego, 
+	true - udalo sie wyeksportowac dane do pliku zewnetrzego,
 	false - nie udalo sie
 
 */
-	bool wypiszListeUczniow (std::string oddzial_csv); 
-	
+	bool wypiszListeUczniow (std::string oddzial_csv);
+
 	private:
 	static const int  max_licz_uczniow = 22;
 	int liczba_uczniow_w_oddziale;
 	Uczen lista_uczniow_w_oddziale[max_licz_uczniow];
-	
-	void segregujUczniow (int liczba_uczniow, Uczen tab[]);
-	
 
+	void segregujUczniow (int liczba_uczniow, Uczen tab[]);
+
+// hello tutaj kacper
 
 };
 #endif
