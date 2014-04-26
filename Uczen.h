da96dc9db32ba5b19ce6947440c9c9aaa309183c
Cahir307 <tomasz_kasprzyk@wp.pl>
2014-04-26 11:26:11 +0200

    uczen cd..
    kosmetyka

--- Uczen.h
+++ Uczen.h
@@ -1,7 +1,6 @@
 #ifndef _UCZEN_H_
 #define _UCZEN_H_
 
-
 #include <string>
 
 class Uczen
@@ -15,26 +14,17 @@ class Uczen
 		std::string imie;
 		int punkty;
 		static const int liczba_wyborow = 3;
-		wybor wyborUcznia[liczba_wyborow];	// TODO: magic number
+		wybor wyborUcznia[liczba_wyborow];
 		jezyk wyborJezyka;
 
-
-
 	public:
-			// metoda podaj_nazwisko - zwraca stringa zawierajacego nazwisko i imie ucznia;
 		std::string podaj_nazwisko();
-			/* metoda liczba_punktow -
-			Wartosc zwracana: liczbe punktow ucznia; */
 		int podaj_liczba_punktow();
-			/* metoda podaj_jezyk -
-			Wartosc zwracana: jezyka ucznia - 0 dla angielski, 1 dla niemiecki; */
 		jezyk podaj_jezyk();
-			/* metoda podaj_wybor -
-			Argumenty wejscia: int ktory - przyjmuje wartosc wyboru oddzialu, ktory preferuje uczen.
-			Wartosc zwracana: zwraca wybor odzialu preferowany przez ucznia; */
 		wybor podaj_wybor(int ktoryWybor);
-
 		Uczen();
-		Uczen(std::string aImie, std::string aNazwisko, int aPunkty, jezyk aJezyk, wybor aWybor1, wybor aWybor2, wybor aWybor3); //konstruktor
+
+		Uczen(std::string aImie, std::string aNazwisko, int aPunkty,
+        jezyk aJezyk, wybor aWybor1, wybor aWybor2, wybor aWybor3);
 };
 #endif
