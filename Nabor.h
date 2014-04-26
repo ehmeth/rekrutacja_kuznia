9caa54bebb4017618f4fa76afc852fe02a84524a
JanStoltman <stoltmanjan@gmail.com>
2014-04-26 11:34:21 +0200

    Kosmetyka

--- Nabor.h
+++ Nabor.h
@@ -2,27 +2,27 @@
 #define _NABOR_H
 
 #include "Uczen.h"
-
 #include <vector>
+
 class Nabor
 {
 	public:
 		enum indeksyDanychPlik{ ImieEnum = 0, NazwiskoEnum, punktyEnum,
-		wybor_1, wybor_2, wybor_3, jezykEnum };
+									wybor_1, wybor_2, wybor_3, jezykEnum };
 
-	int ilosc();// Zwraca ilosc uczniow w tabelii
-
+	int ilosc();
+		 // Zwraca ilosc uczniow w tabelii
 
 	bool podaj_ucznia( int numerUcznia, Uczen * pUczen);
-												//Pobiera indeks ucznia i wskaünik na zmiennπ typu Uczen
-												// zwrafa false w przypadku braku danego wiersza w tabeli i true gdy iwersz istnieje
-	Nabor(std::string sciezkaDoPliku);//Pobiera sciezke do pliku i tworzy tabele uczniow posortowana wedlug punktow;
-	                                //osoby z niemieckim jako podstawowym umieszczane sa na poczatku tabeli
+		//Pobiera indeks ucznia i wskaünik na zmiennπ typu Uczen zwraca false w przypadku braku danego wiersza w tabeli i true gdy wiersz istnieje
+
+	Nabor(std::string sciezkaDoPliku);
+	   //Pobiera sciezke do pliku i tworzy tabele uczniow posortowana wedlug punktow osoby z niemieckim jako podstawowym umieszczane sa na poczatku tabeli
+
 
 	private:
 	std::vector <Uczen> tabUczniow;
 	int ileWersow;
-	//int punktyZaOceny[5] = { 1 };
 
 	Uczen::wybor strDoWybor(std::string str);
 	Uczen::jezyk strDoJezyk(std::string str);
